f5f03e31109a5e8783fba2952269d292c1e2ee20
Fotis Papadogeorgopoulos <fotis@futurice.com>
2021-03-12 11:30:44 +0200

    Start getting production working

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,20 @@ import legacy from "@vitejs/plugin-legacy";
 export default defineConfig({
   plugins: [legacy],
   build: {
-    // generate manifest.json in outDir
+    // This is important: Generate directly to _site and then assetsDir.
+    // You could opt to build in an intermediate directory,
+    // and have Eleventy copy the flies instead.
+    outDir: "_site",
+    // This is the default assetsDir. If you are using assets
+    // for anything else, consider renaming assetsDir.
+    // This can help you set cache headers for hashed output more easily.
+    // assetsDir: "assets",
+    // Sourcemaps are nice, but not critical for this to work
+    sourcemap: true,
+    // This is critical: generate manifest.json in outDir
     manifest: true,
     rollupOptions: {
-      // overwrite default .html entry
+      // This is critical: overwrite default .html entry
       input: "/src/client/main.js",
     },
   },
