85996f7784db60c6a59f706a5607c583260f03e8
Fotis Papadogeorgopoulos <fotis@futurice.com>
2021-03-30 13:10:40 +0300

    Use css import in main file

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,10 +1,11 @@
-import "vite/dynamic-import-polyfill";
-import App from "./App";
-import React from "react";
-import { render } from "react-dom";
+import 'vite/dynamic-import-polyfill';
+import App from './App';
+import React from 'react';
+import {render} from 'react-dom';
+import './css/index.css';
 
 function init() {
-  const rootNode = document.getElementById("react-root");
+  const rootNode = document.getElementById('react-root');
   render(<App />, rootNode);
 }
 
